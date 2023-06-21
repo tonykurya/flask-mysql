1251598cd0a0d42cf99e771e2d6a1f5e319e8c7c
Toni <tonykurya@gmail.com>
2023-06-21 13:15:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import jsonify, request, Flask
 from flaskext.mysql import MySQL
@@ -23,15 +24,15 @@ def index():
 
 @app.route("/create", methods=["POST"])
 def add_user():
-    """Function to create a user in the MySQL database"""
+    """Function to create a user to the MySQL database"""
     json = request.json
     name = json["name"]
     email = json["email"]
-    bio = json["bio"]
-    if name and email and bio and request.method == "POST":
-        sql = "INSERT INTO users(name, email, short_bio) " \
-      "VALUES(%s, %s, %s)"
-        data = (name, email, bio)
+    pwd = json["pwd"]
+    if name and email and pwd and request.method == "POST":
+        sql = "INSERT INTO users(user_name, user_email, user_password) " \
+              "VALUES(%s, %s, %s)"
+        data = (name, email, pwd)
         try:
             conn = mysql.connect()
             cursor = conn.cursor()
@@ -45,7 +46,7 @@ def add_user():
         except Exception as exception:
             return jsonify(str(exception))
     else:
-        return jsonify("Please provide name, email, and bio")
+        return jsonify("Please provide name, email and pwd")
 
 
 @app.route("/users", methods=["GET"])
@@ -65,51 +66,67 @@ def users():
         return jsonify(str(exception))
 
 
-@app.route("/user/<int:id>")
-def user(id):
-    """Function to get information of a specific user from the MySQL database"""
+@app.route("/user/<int:user_id>", methods=["GET"])
+def user(user_id):
+    """Function to get information of a specific user in the MSQL database"""
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE id=%s", (id,))
+        cursor.execute("SELECT * FROM users WHERE user_id=%s", user_id)
         row = cursor.fetchone()
         cursor.close()
         conn.close()
-        if row:
-            resp = jsonify(row)
-            resp.status_code = 200
-            return resp
-        else:
-            return jsonify("User not found")
+        resp = jsonify(row)
+        resp.status_code = 200
+        return resp
     except Exception as exception:
         return jsonify(str(exception))
 
 
 @app.route("/update", methods=["POST"])
 def update_user():
-    """Function to update a user in the MySQL database"""
+    """Function to update a user in the MYSQL database"""
     json = request.json
     name = json["name"]
     email = json["email"]
-    bio = json["bio"]
-    id = json["id"]
-    if name and email and bio and id and request.method == "POST":
-        sql = "UPDATE users SET name=%s, email=%s, short_bio=%s WHERE id=%s"
-        data = (name, email, bio, id)
+    pwd = json["pwd"]
+    user_id = json["user_id"]
+    if name and email and pwd and user_id and request.method == "POST":
+        # save edits
+        sql = "UPDATE users SET user_name=%s, user_email=%s, " \
+              "user_password=%s WHERE user_id=%s"
+        data = (name, email, pwd, user_id)
         try:
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
             conn.commit()
-            cursor.close()
-            conn.close()
             resp = jsonify("User updated successfully!")
             resp.status_code = 200
+            cursor.close()
+            conn.close()
             return resp
         except Exception as exception:
             return jsonify(str(exception))
     else:
-        return jsonify("Please provide id, name, email, and bio")
+        return jsonify("Please provide id, name, email and pwd")
+
+
+@app.route("/delete/<int:user_id>")
+def delete_user(user_id):
+    """Function to delete a user from the MySQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM users WHERE user_id=%s", user_id)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        resp = jsonify("User deleted successfully!")
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
 
 
 if __name__ == "__main__":
